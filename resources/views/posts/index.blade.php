5c6a1d8a2fd052b3c60b940988b287be67ef85fb
wasao123 <jobhunting0414@gmail.com>
2022-05-29 17:13:29 +0900

    nidodema

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,11 +8,13 @@
     </head>
     <body>
         <h1>Blog Name</h1>
+
         <p class="create">[<a href='/posts/create'>create</a>]</p>
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
                     <h2 class='title'><a href="/posts/{{ $post->id }}/edit">{{ $post->title }}</a></h2>
+
                     <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach
